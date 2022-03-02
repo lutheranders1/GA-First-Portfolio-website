9445af2c7e0742340632a4a91210d8aa2e8efb98
lutheranders1 <lutheranders@gmail.com>
2022-03-02 12:17:49 -0600

    Completed Porject

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
-$(document).ready(function(){
 let accord = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < accord.length; i++) {
-  accord[i].addEventListener("click", function() {
+  accord[i].addEventListener("click", function () {
     this.classList.toggle("active");
-     let panel = this.nextElementSibling;
-     if (panel.style.maxHeight) {
-       panel.style.maxHeight = null;
-     } else {
-      panel.style.maxHeight = panel.scrollHeight+"px";
-      panel.style.maxWidth = panel.scrollWidth+"px";
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxWidth = panel.scrollWidth + "px";
       panel.style.overflow = "scroll";
-     }
+    }
   });
 }
 
 /****************************************************** */
 const formsub = document.querySelector(".main-form");
 
-formsub.addEventListener('submit', e => {
-
-alert(`Thank you for submitting, I'll be in touch soon `)})
+formsub.addEventListener("submit", (e) => {
+  alert(`Thank you for submitting, I'll be in touch soon `);
+});
 
 /****************************************************/
+function compute() {
+  let principal = document.getElementById("principal").value;
 
-$("#btn").click(function compute() {
-  
-{
-    let principal = $("#principal").value();
-
-    if(principal == "" || principal <= 0)
-    {
-        alert("Enter a positive number");
-        
-        return;
-    }
-    else {
-    let rate = $("#rate").value();
-    let years = $("#years").value();
-    let interest = principal * years * rate / 100;
-
-    let dateNow = new Date();
-    let yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
-    
-    
-    $("#result").html=(`If you deposit  ${principal} at an interest rate of ${rate}  You will receive an amount of  ${interest} in the year ${yearNow}`);
+  if (principal == "" || principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return;
   }
-}
-});});
 
-$("#rate").change(function slider(){
-  
-  $("#rate_val").html = $("#rate").value();
-   
-});
- 
+  let rate = document.getElementById("rate").value;
+  let years = document.getElementById("years").value;
+  let interest = (principal * years * rate) / 100;
+
+  let dateNow = new Date();
+  let yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+
+  let rate_val = document.getElementById("result");
+
+  let answer = parseInt(principal) + parseInt(interest);
+  document.getElementById(
+    "result"
+  ).innerHTML = `If you deposit ${principal} at a rate of ${rate}% you will receive $${answer} dollars in year ${yearNow}`;
+}
+
+function SliderValue() {
+  var slider = document.getElementById("rate");
+  var output = document.getElementById("rate_val");
+  output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.onchange = function () {
+    output.innerHTML = this.value;
+  };
+}
